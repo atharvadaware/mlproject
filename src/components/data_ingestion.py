1501c74ee88aeabb4f18b2af27255f6e42062c12
Atharv <atharvadaware2001@gmail.com>
2025-08-11 15:16:46 +0530

    Training pipeline updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -53,12 +53,3 @@ class DataIngestion:
         
 
 
-if __name__=='__main__':
-    obj=DataIngestion()
-    train_data, test_data=obj.initiate_data_ingestion()
-
-    data_transformation=DataTransformation()
-    train_arr, test_arr= data_transformation.initiate_data_transformation(test_data, test_data)
-
-    model_trainer=ModelTrainer()
-    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
--- src/pipelines/train_pipeline.py
+++ src/pipelines/train_pipeline.py
@@ -1,1 +1,19 @@
+from  src.logger import logging
+from src.exception import CustomException
+from src.components.data_ingestion import DataIngestionConfig
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
+
+if __name__=='__main__':
+    data_ingestion=DataIngestion()
+    train_data, test_data=data_ingestion.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr, test_arr= data_transformation.initiate_data_transformation(test_data, test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
